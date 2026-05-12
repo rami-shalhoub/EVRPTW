2b01ee58d067e7a39b4a200aa0b30f919579c70c
rami-shalhoub <rami2002.work@gmail.com>
2026-05-12 12:43:24 +0200

    - move the export function to `helpers.py` and rename it `export_to_csv` - update `helpers.format_number` to round to a specific number, default   2 like the csv file provided from the professor - add static typing to variables in `main.py` - remove abundant from `main.py`

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.helpers import export, total_cost, route_cost, format_number
+from src.helpers import  export_to_csv, total_cost
 from src.instances import get_instances
 from src.localSearch import local_search
 from src.solutionConstructor import greedy_construction
@@ -6,7 +6,6 @@ from src.solutionConstructor import greedy_construction
 import random
 import os
 import time
-import csv
 from statistics import mean
 
 RUNS= 5
@@ -20,29 +19,27 @@ if __name__ == "__main__":
     instance_folder ="./resources/instances/"
     instance_files = [f for f in os.listdir(instance_folder) if f.endswith(".txt")]
 
-    results= []
+    results:list[tuple[str, float, float, float]]= []
 
     for file in instance_files:
         path= os.path.join(instance_folder, file)
         inst = get_instances(path)
         instance_name = file.replace(".txt", "")
-        final_costs = []
-        total_times = []
+        final_costs: list[float] = []
+        total_times:list[float] = []
 
         print(f"\nRunning {instance_name} for {RUNS} runs...")
 
         for i in range(RUNS):
-            random.seed(i)
-
             #construction + local search
 
             start = time.perf_counter()
 
-            routes = greedy_construction(inst, 1000, 5, 2)
+            routes = greedy_construction(inst, 1000, RUNS, 2)
 
             if routes is not None:
                 routes = local_search(routes, inst)
-                cost = total_cost(routes)
+                # cost = total_cost(routes)
                 final_cost = total_cost(routes)
                 final_costs.append(final_cost)
                 runtime = time.perf_counter() - start
@@ -59,9 +56,4 @@ if __name__ == "__main__":
         results.append((instance_name, best_final, avg_final, avg_time))   
 
     #Output in CSV file
-    with open('results_3.csv', mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file, delimiter=";")
-        writer.writerow(["", "best", "avg", "avg.time(s)"])
-
-        for r in results:
-            writer.writerow([r[0], format_number(r[1]), format_number(r[2]), format_number(r[3])]) 
+    export_to_csv(results, 'results_2')
